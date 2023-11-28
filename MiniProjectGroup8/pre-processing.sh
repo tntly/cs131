3d55ab5a4d86395e9a97c79bab9a843266e07c56
Tien Ly <tntly@ucdavis.edu>
2023-11-27 23:48:36 -0800

    fixed format

--- MiniProjectGroup8/pre-processing.sh
+++ MiniProjectGroup8/pre-processing.sh
@@ -1,7 +1,10 @@
 #!bin/bash
 
-awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; OFS = ","} $11 == "chinese" {print $2, $3, $4, $5, $6, $7, $8, $9, $10}' east_asia_top_tracks.csv | awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} $5 >= 70 {print}' > chinese
+awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; OFS = ","} $11 == "chinese" {print $2, $3, $4, $5, $6, $7, $8, $9, $10}' east_asia_top_tracks.csv | \
+awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} $5 >= 70 {print}' > chinese
 
-awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; OFS = ","} $11 == "j-pop" || $11 == "japanese" || $11 == "j-dance" || $11 == "j-idol" {print $2, $3, $4, $5, $6, $7, $8, $9, $10}' east_asia_top_tracks.csv | sort -u | awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} $5 >= 70 {print}' > japanese
+awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; OFS = ","} $11 == "j-pop" || $11 == "japanese" || $11 == "j-dance" || $11 == "j-idol" {print $2, $3, $4, $5, $6, $7, $8, $9, $10}' east_asia_top_tracks.csv | \
+sort -u | awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} $5 >= 70 {print}' > japanese
 
-awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; OFS = ","} $11 == "k-pop" || $11 == "korean" {print $2, $3, $4, $5, $6, $7, $8, $9, $10}' east_asia_top_tracks.csv | sort -u | awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} $5 >= 70 {print}' > korean
+awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; OFS = ","} $11 == "k-pop" || $11 == "korean" {print $2, $3, $4, $5, $6, $7, $8, $9, $10}' east_asia_top_tracks.csv | \
+sort -u | awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} $5 >= 70 {print}' > korean
