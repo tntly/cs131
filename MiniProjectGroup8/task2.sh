78c4153d8d360d5f280bc16b47623f37bc411349
Tien Ly <tntly@ucdavis.edu>
2023-11-28 03:32:18 -0500

    fixing task 2.2

--- MiniProjectGroup8/task2.sh
+++ MiniProjectGroup8/task2.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-echo 'task 2.1' > task2.txt
+echo 'Task 2.1' > task2.txt
 # task 2.1: list the top 5 artists with the most songs for each file
 awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} {print $4}' chinese | sort | uniq -c | sort -nr | head -n 5 | awk 'BEGIN {print "Top 5 Chinese Artists with the Most Songs:"} {print} END {printf "\n"}' >> task2.txt
 
@@ -8,10 +8,10 @@ awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} {print $4}' japanese | sort | 
 
 awk 'BEGIN {FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"} {print $4}' korean | sort | uniq -c | sort -nr | head -n 5 | awk 'BEGIN {print "Top 5 Korean Artists with the Most Songs:"} {print} END {printf "\n"}' >> task2.txt
 
-echo 'task 2.2' >> task2.txt
+echo 'Task 2.2' >> task2.txt
 # task 2.2: list the top 5 oldest songs in each file
 sort -t, -k6,6 chinese | head -n 5 | awk -F, 'BEGIN {OFS = ","; print "Top 5 Oldest Chinese Songs:"} {print $1, $2, $5, $6} END {printf "\n"}' >> task2.txt
 
 sort -t, -k6,6 japanese | head -n 5 | awk -F, 'BEGIN {OFS = ","; print "Top 5 Oldest Japanese Songs:"} {print $1, $2, $5, $6} END {printf "\n"}' >> task2.txt
 
-sort -t, -k6,6 japanese | head -n 5 | awk -F, 'BEGIN {OFS = ","; print "Top 5 Oldest Japanese Songs:"} {print $1, $2, $5, $6}' >> task2.txt
+sort -t, -k6,6 korean | head -n 5 | awk -F, 'BEGIN {OFS = ","; print "Top 5 Oldest Korean Songs:"} {print $1, $2, $5, $6}' >> task2.txt
